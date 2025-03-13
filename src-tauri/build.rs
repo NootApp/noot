5299464e7a8c14c7af247039e7feb149016c8da4
Arthur Amos <arthur@totallyfake.dev>
2025-03-13 01:07:49 +0000

    Nuke old Tauri source code

--- src-tauri/build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    println!("cargo::rustc-check-cfg=cfg(test)");
-    tauri_build::build()
-}
--- src-tauri/src/filesystem/watcher.rs
+++
@@ -1,8 +0,0 @@
-
-
-
-//pub fn watch_dir_for_changes(path: &PathBuf) {
-//    std::thread::spawn(|| {
-//
-//    })
-//}
